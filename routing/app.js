48bae80a1905b8f6b8d12d731d3815f4792d8800
Rob Ashton <robashton@codeofrob.com>
2013-12-04 16:54:23 +0000

    Made routing a little less manual worky

--- routing/app.js
+++ routing/app.js
@@ -2,17 +2,26 @@ var domReady = require('domready')
   , data = require('./testdata')
   , mustache = require('mustache')
   , fs = require('fs')
-  , template = fs.readFileSync(__dirname + '/template.html')
+  , poniesTemplate = fs.readFileSync(__dirname + '/template.html')
+  , unicornTemplate = fs.readFileSync(__dirname + '/unicorns.html')
   , Delegate = require('dom-delegate')
   , dope = require('dope')
+  , LocationBar = require('location-bar')
 
 domReady(function() {
   var container = document.getElementById('container')
-  container.innerHTML = mustache.render(template, data)
-
   var delegate = new Delegate(container)
+
   delegate.on('click', '.pony', function(e, r) {
     console.log(dope.dataset(r).pony)
   })
 
+  document.getElementById('ponies').onclick = function() {
+
+    return false
+  }
+  document.getElementById('unicorns').onclick = function() {
+
+    return false
+  }
 })
--- routing/testdata/index.js
+++ routing/testdata/index.js
@@ -3,5 +3,10 @@ module.exports = {
     { name: "Pinkie Pie", trampstamp: "Balloons" },
     { name: "Rainbow Dash", trampstamp: "Clouds" },
     { name: "Apple jack", trampstamp: "Apples" }
+  ],
+  unicorns: [
+    { name: "Princess Celestia", trampstamp: "Sun" },
+    { name: "Rarity", trampstamp: "Diamons" },
+    { name: "Twilight Sparkle", trampstamp: "Sparkles" }
   ]
 }
